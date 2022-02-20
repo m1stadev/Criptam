4cb0dd366d9a7d8dc04292bc1c55ceec628c01ae
adam <adamhamdi31@gmail.com>
2022-02-20 13:42:23 -0600

    Criptam v1.0.2b (#2)
    * [main] Update tool description
    
    * Add beta support
    
    * Bump version to 1.0b2
    
    * Update README
    
    * Hide ipwndfu pwn output
    
    * Remove prompt to connect DFU device
    
    * Make install script executable

--- criptam/__main__.py
+++ criptam/__main__.py
@@ -12,10 +12,13 @@ import sys
 
 __version__ = version(__package__)
 
+RELEASE_API = 'https://api.ipsw.me/v4/device'
+BETA_API = 'https://api.m1sta.xyz/betas'
+
 
 def main():
     parser = argparse.ArgumentParser(
-        description=f'Criptam {__version__} - Automatic Firmware Key decryptor',
+        description=f'Criptam {__version__} - iOS firmware key decrypter',
         usage="criptam -b 'buildid'",
     )
 
@@ -29,49 +32,61 @@ def main():
         sys.exit('[ERROR] Windows systems are not supported. Exiting.')
 
     print(f'Criptam {__version__}')
-    input('\nPlease connect an iOS device in DFU mode to your PC, then press enter.')
     device = Device()
 
     buildid = args.buildid.upper()
     print(f'\nGetting IPSW URL for build: {buildid}')
-    api = requests.get(
-        f"https://api.ipsw.me/v4/device/{device.data['identifier']}?type=ipsw"
-    ).json()
 
-    try:
-        ipsw = IPSW(
-            device,
-            next(_['url'] for _ in api['firmwares'] if _['buildid'] == buildid),
-        )
-    except StopIteration:
+    firm = None
+    for API_URL in (RELEASE_API, BETA_API):
+        api = requests.get(f"{API_URL}/{device.data['identifier']}").json()
+
+        if API_URL == RELEASE_API:
+            firmwares = api['firmwares']
+        elif API_URL == BETA_API:
+            firmwares = api
+
+        try:
+            firm = next(
+                firm for firm in firmwares if firm['buildid'].lower() == buildid.lower()
+            )
+        except StopIteration:
+            pass
+
+    if firm is None:
         sys.exit(
             f"iOS Build {buildid} does not exist for device: {device.data['identifier']}. Exiting."
         )
+
+    ipsw = IPSW(
+        device,
+        firm['url'],
+    )
 
     if not device.pwned:
         print('Entering Pwned DFU mode...')
         device.pwn()
 
     print(
-        f"Decrypting keys for iOS build: {buildid}, device: {device.data['identifier']}..."
+        f"Decrypting keys for iOS {firm['version']}, device: {device.data['identifier']}..."
     )
     ibss = ipsw.read_file(ipsw.manifest.get_path('iBSS'))
     ibec = ipsw.read_file(ipsw.manifest.get_path('iBEC'))
     llb = ipsw.read_file(ipsw.manifest.get_path('LLB'))
     iboot = ipsw.read_file(ipsw.manifest.get_path('iBoot'))
 
-    decryptor = Decrypt(device)
+    decrypter = Decrypt(device)
 
-    ibss_iv, ibss_key = decryptor.decrypt_keys(ibss)
+    ibss_iv, ibss_key = decrypter.decrypt_keys(ibss)
     print(f'\niBSS KBAG: {ibss_iv + ibss_key}')
 
-    ibec_iv, ibec_key = decryptor.decrypt_keys(ibec)
+    ibec_iv, ibec_key = decrypter.decrypt_keys(ibec)
     print(f'iBEC KBAG: {ibec_iv + ibec_key}')
 
-    llb_iv, llb_key = decryptor.decrypt_keys(llb)
+    llb_iv, llb_key = decrypter.decrypt_keys(llb)
     print(f'LLB KBAG: {llb_iv + llb_key}')
 
-    iboot_iv, iboot_key = decryptor.decrypt_keys(iboot)
+    iboot_iv, iboot_key = decrypter.decrypt_keys(iboot)
     print(f'iBoot KBAG: {llb_iv + llb_key}')
 
     print('\nDone!')
