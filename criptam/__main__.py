1d384d71b2e45868eafc66cfab577c26a5a4bd59
adam <adamhamdi31@gmail.com>
2022-03-11 22:28:25 -0600

    Criptam v1.0b3 (#3)
    * [main] Update tool description
    
    * Add beta support
    
    * Bump version to 1.0b2
    
    * Update README
    
    * Hide ipwndfu pwn output
    
    * Remove prompt to connect DFU device
    
    * Make install script executable
    
    * Bump script version
    
    * [IPSW] Update
    - Update type-hinting
    - read_file: Error handling
    
    * [decrypt] decrypt_keys: Fix return
    
    * [ipsw] read_file: Return None if RemoteIOError
    
    * [main] Update
    - Update help
    - Add decrypting all keys for a major version
    - Add decrypting all keys
    - Add error handling for if IPSW is inaccessible
    
    * Update README

--- criptam/__main__.py
+++ criptam/__main__.py
@@ -19,77 +19,126 @@ BETA_API = 'https://api.m1sta.xyz/betas'
 def main():
     parser = argparse.ArgumentParser(
         description=f'Criptam {__version__} - iOS firmware key decrypter',
-        usage="criptam -b 'buildid'",
+        usage="criptam [-b BUILDID] [-m MAJOR] [-a]",
     )
 
-    parser.add_argument('-b', '--buildid', help='iOS build to decrypt keys for')
-    args = parser.parse_args()
+    version = parser.add_mutually_exclusive_group(required=True)
+    version.add_argument(
+        '-b', '--buildid', help='iOS build to decrypt firmware keys for'
+    )
+    version.add_argument(
+        '-m', '--major', help='Major iOS version to decrypt firmware keys for'
+    )
+    version.add_argument(
+        '-a',
+        '--all',
+        help='Decrypt firmware keys for all versions',
+        action='store_true',
+    )
 
-    if not args.buildid:
-        sys.exit(parser.print_help(sys.stderr))
+    args = parser.parse_args()
 
     if platform.system() == 'Windows':
         sys.exit('[ERROR] Windows systems are not supported. Exiting.')
 
     print(f'Criptam {__version__}')
+
+    print(f'\nConnecting to DFU device...')
     device = Device()
 
-    buildid = args.buildid.upper()
-    print(f'\nGetting IPSW URL for build: {buildid}')
+    print(f"\nGetting firmware information for device: {device.data['identifier']}...")
 
-    firm = None
+    firmwares = list()
     for API_URL in (RELEASE_API, BETA_API):
         api = requests.get(f"{API_URL}/{device.data['identifier']}").json()
 
         if API_URL == RELEASE_API:
-            firmwares = api['firmwares']
+            firms = api['firmwares']
         elif API_URL == BETA_API:
-            firmwares = api
+            firms = api
 
-        try:
-            firm = next(
-                firm for firm in firmwares if firm['buildid'].lower() == buildid.lower()
+        for firm in firms:
+            if any(firm['buildid'] == f['buildid'] for f in firmwares):
+                continue
+
+            firmwares.append(firm)
+
+    firmwares = sorted(firmwares, key=lambda x: x['buildid'], reverse=True)
+    if args.buildid:
+        buildid = args.buildid.upper()
+        firmwares = [
+            firm
+            for firm in firmwares
+            if firm['buildid'].casefold() == buildid.casefold()
+        ]
+        if len(firmwares) == 0:
+            sys.exit(
+                f"iOS Build {buildid} does not exist for device: {device.data['identifier']}. Exiting."
             )
-        except StopIteration:
-            pass
 
-    if firm is None:
-        sys.exit(
-            f"iOS Build {buildid} does not exist for device: {device.data['identifier']}. Exiting."
-        )
+    elif args.major:
+        firmwares = [
+            firm for firm in firmwares if firm['version'].startswith(args.major)
+        ]
+        if len(firmwares) == 0:
+            sys.exit(
+                f"iOS {args.major} does not exist for device: {device.data['identifier']}. Exiting."
+            )
 
-    ipsw = IPSW(
-        device,
-        firm['url'],
-    )
+    elif args.all:
+        pass
+
+    if len(firmwares) > 10:
+        try:
+            input(
+                f"[WARNING] This will decrypt bootchain firmware keys for {len(firmwares)} firmwares.\nPress ENTER to continue, or CTRL+C to cancel: "
+            )
+        except KeyboardInterrupt:
+            sys.exit('\nExiting.')
 
     if not device.pwned:
         print('Entering Pwned DFU mode...')
         device.pwn()
 
-    print(
-        f"Decrypting keys for iOS {firm['version']}, device: {device.data['identifier']}..."
-    )
-    ibss = ipsw.read_file(ipsw.manifest.get_path('iBSS'))
-    ibec = ipsw.read_file(ipsw.manifest.get_path('iBEC'))
-    llb = ipsw.read_file(ipsw.manifest.get_path('LLB'))
-    iboot = ipsw.read_file(ipsw.manifest.get_path('iBoot'))
+    for firm in firmwares:
+        try:
+            ipsw = IPSW(
+                device,
+                firm['url'],
+            )
+        except:
+            if len(firmwares) == 1:
+                sys.exit(
+                    f"[ERROR] Failed to download bootchain for iOS {firm['version']}, device: {device.data['identifier']}. Exiting."
+                )
+            else:
+                print(
+                    f"[ERROR] Failed to download bootchain for iOS {firm['version']}, device: {device.data['identifier']}. Skipping.\n"
+                )
 
-    decrypter = Decrypt(device)
+        print(
+            f"Decrypting keys for iOS {firm['version']}, device: {device.data['identifier']}..."
+        )
+        ibss = ipsw.read_file(ipsw.manifest.get_path('iBSS'))
+        ibec = ipsw.read_file(ipsw.manifest.get_path('iBEC'))
+        llb = ipsw.read_file(ipsw.manifest.get_path('LLB'))
+        iboot = ipsw.read_file(ipsw.manifest.get_path('iBoot'))
 
-    ibss_iv, ibss_key = decrypter.decrypt_keys(ibss)
-    print(f'\niBSS KBAG: {ibss_iv + ibss_key}')
+        decrypter = Decrypt(device)
 
-    ibec_iv, ibec_key = decrypter.decrypt_keys(ibec)
-    print(f'iBEC KBAG: {ibec_iv + ibec_key}')
+        ibss_iv, ibss_key = decrypter.decrypt_keys(ibss)
+        print(f"\niBSS KBAG for iOS {firm['version']}: {ibss_iv + ibss_key}")
 
-    llb_iv, llb_key = decrypter.decrypt_keys(llb)
-    print(f'LLB KBAG: {llb_iv + llb_key}')
+        ibec_iv, ibec_key = decrypter.decrypt_keys(ibec)
+        print(f"iBEC KBAG for iOS {firm['version']}: {ibec_iv + ibec_key}")
 
-    iboot_iv, iboot_key = decrypter.decrypt_keys(iboot)
-    print(f'iBoot KBAG: {llb_iv + llb_key}')
+        llb_iv, llb_key = decrypter.decrypt_keys(llb)
+        print(f"LLB KBAG for iOS {firm['version']}: {llb_iv + llb_key}")
 
-    print('\nDone!')
+        iboot_iv, iboot_key = decrypter.decrypt_keys(iboot)
+        print(f"iBoot KBAG for iOS {firm['version']}: {iboot_iv + iboot_key}\n")
+
+    print('Done!')
 
 
 if __name__ == '__main__':
